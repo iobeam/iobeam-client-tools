48be40137946ff800842c640a2417588dfce0971
Michael J. Freedman <mfreed@cs.princeton.edu>
2015-12-18 20:27:13 -0500

    Add config class, move all checks to before device registration

--- data-uploader.py
+++ data-uploader.py
@@ -4,9 +4,14 @@ import sys
 
 from iobeam import iobeam
 
-# To Edit
-IOBEAM_PROJECT_ID = None
+###############################################################
+# Set project ID and token in source code, or from command line
+###############################################################
+
+IOBEAM_PROJECT_ID = None 
 IOBEAM_TOKEN = None
+
+###############################################################
 
 _parser = argparse.ArgumentParser(version='0.1',
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter,
@@ -25,38 +30,39 @@ its granularity (sec,msec,usec) should be specified as a program arg.
 
 ''')
 
-# Do not modify
-_iobeamClient = None
-_iobeamDataStore = None
+###############################################################
 
-_format = None
-_formatWithoutTimestamp = None
-_timestampIndex = -1
-_timestampFormat = None
-_multiplier = None
+class Config:
+    def __init__(self, args):
+        # Do not modify
+        self.args = args
+        self.iobeamClient = None
+        self.iobeamDataStore = None
 
-def addData(data):
-    global _iobeamDataStore
-    global _format
-    global _formatWithoutTimestamp
-    global _timestampIndex
-    global _multiplier
+        self.format = None
+        self.formatWithoutTimestamp = None
+        self.timestampIndex = -1
+        self.timestampFormat = None
+        self.multiplier = None
 
-    if len(_format) != len(data):
+
+def addData(config, data):
+
+    if len(config.format) != len(data):
         print "\nError: Number of data elements does not match format"
-        print "\tFormat:\t%s" % _format
+        print "\tFormat:\t%s" % config.format
         print "\tData:\t%s" % data
         sys.exit(1)
 
-    if _timestampIndex < 0:
-        now = int(time.time() * _multiplier)
-        ts = iobeam.Timestamp(now, _timestampFormat)
-        _iobeamDataStore.add(ts, dict(zip(_format, data)))
+    if config.timestampIndex < 0:
+        now = int(time.time() * config.multiplier)
+        ts = iobeam.Timestamp(now, config.timestampFormat)
+        config.iobeamDataStore.add(ts, dict(zip(config.format, data)))
     else:
-        now = int(data[_timestampIndex])
-        ts = _timestampFormat(now, _timestampFormat)
-        del data[_timestampIndex]
-        _iobeamDataStore.add(ts, dict(zip(_formatWithoutTimestamp, data)))
+        now = int(data[config.timestampIndex])
+        ts = config.timestampFormat(now, config.timestampFormat)
+        del data[config.timestampIndex]
+        config.iobeamDataStore.add(ts, dict(zip(config.formatWithoutTimestamp, data)))
 
 
 def getDataFormat(line):
@@ -75,44 +81,83 @@ def getDataLine(line):
     return map((lambda x: x.strip()), line.split(','))
 
 
-def analyzeFile(args):
-    global _iobeamClient
-    global _iobeamDataStore
-    global _format
-    global _formatWithoutTimestamp
-    global _timestampIndex
-
+def analyzeFile(config):
     try:
-        with open(args.input_file, 'r') as f:
-            first_line = f.readline()
-            if _iobeamDataStore == None or _format == None:
-                _format = getDataFormat(first_line)
-                _iobeamDataStore = _iobeamClient.createDataStore(_format)
+        with open(config.args.input_file, 'r') as f:
+            # first_line is formatting.  Read past it.
+            f.readline()
 
-                if 'timestamp' in _format:
-                    _timestampIndex = _format.index('timestamp')
-                    _formatWithoutTimestamp = list(_format)
-                    del _formatWithoutTimestamp[_timestampIndex]
-
-            cnt = args.rows_per
+            cnt = config.args.rows_per
             for line in f:
-                addData(getDataLine(line))
-                if args.rows_per > 0 and cnt == 0:
-                    _iobeamClient.send()
+                addData(config, getDataLine(line))
+                if config.args.rows_per > 0 and cnt == 0:
+                    config.iobeamClient.send()
                     print "Sent data batch to iobeam"
-                    time.sleep((args.delay_bw / 1000))
-                    cnt = args.rows_per
+                    time.sleep((config.args.delay_bw / 1000.0))
+                    cnt = config.args.rows_per
                 cnt -= 1
 
-            _iobeamClient.send()
+            config.iobeamClient.send()
             print "Sent data batch to iobeam"
-            time.sleep((args.delay_bw / 1000))
+            time.sleep((config.args.delay_bw / 1000.0))
 
 
     except (OSError, IOError) as e:
-        __parser.print_usage()
+        _parser.print_usage()
         print "\nError reading file."
         sys.exit(1)
+
+
+def extractFileFormat(config):
+    try:
+        with open(config.args.input_file, 'r') as f:
+            first_line = f.readline()
+            config.format = getDataFormat(first_line)
+
+            if 'timestamp' in config.format:
+                config.timestampIndex = config.format.index('timestamp')
+                config.formatWithoutTimestamp = list(config.format)
+                del config.formatWithoutTimestamp[config.timestampIndex]
+
+    except (OSError, IOError) as e:
+        _parser.print_usage()
+        print "\nError reading file"
+        sys.exit(1)
+
+
+def checkArgs(config):
+
+    error = None
+
+    if config.args.project_id == None:
+        error = "Unknown project ID"
+    elif config.args.token == None:
+        error = "Unknown project token"
+    elif config.args.rows_per < 0:
+        error = "Number of rows must be >= 0"
+    elif config.args.delay_bw < 0:
+        error = "Delay must be >= 0 milliseconds"
+    elif config.args.repetitions < 0:
+        error = "Repetitions must be >= 0"
+    else:
+        config.args.timestamp = config.args.timestamp.lower();
+        if config.args.timestamp == 'sec':
+            config.timestampFormat = iobeam.TimeUnit.SECONDS
+            config.multiplier = 1
+        elif config.args.timestamp == 'msec':
+            config.timestampFormat = iobeam.TimeUnit.MILLISECONDS
+            config.multiplier = 1000
+        elif config.args.timestamp == 'usec':
+            config.timestampFormat = iobeam.TimeUnit.MICROSECONDS
+            config.multiplier = 1000000
+        else:
+            error = "Timestamp must be 'sec', 'msec', or 'usec'"
+
+    if error != None:
+        _parser.print_usage()
+        print "\nError: %s" % error
+        sys.exit(1)
+
 
 
 if __name__ == "__main__":
@@ -135,62 +180,25 @@ if __name__ == "__main__":
                         help='Delay between sending iteration (in milliseconds)', default=1000)
 
 
-    args = _parser.parse_args()
+    config = Config(_parser.parse_args())
+    checkArgs(config)
+    extractFileFormat(config)
 
-    if args.project_id == None:
-        _parser.print_usage()
-        print "\nError: Unknown project ID."
-        sys.exit(1)
-
-    if args.token == None:
-        _parser.print_usage()
-        print "\nError: Unknown project token."
-        sys.exit(1)
-
-    if args.rows_per < 0:
-        _parser.print_usage()
-        print "\nError: Number of rows must be >= 0."
-        sys.exit(1)
-
-    if args.delay_bw < 0:
-        _parser.print_usage()
-        print "\nError: Delay must be >= 0 milliseconds."
-        sys.exit(1)
-
-    args.timestamp = args.timestamp.lower();
-    if args.timestamp == 'sec':
-        _timestampFormat = iobeam.TimeUnit.SECONDS
-        _multiplier = 1
-    elif args.timestamp == 'msec':
-        _timestampFormat = iobeam.TimeUnit.MILLISECONDS
-        _multiplier = 1000
-    elif args.timestamp == 'usec':
-        _timestampFormat = iobeam.TimeUnit.MICROSECONDS
-        _multiplier = 1000000
-    else:
-        _parser.print_usage()
-        print "\nError: Timestamp must be 'sec', 'msec', or 'usec'."
-        sys.exit(1)
-
-
-    builder = iobeam.ClientBuilder(args.project_id, args.token) \
+    builder = iobeam.ClientBuilder(config.args.project_id, config.args.token) \
         .setBackend("https://api-dev.iobeam.com/v1/") \
         .saveToDisk()
 
-    if args.device_id != None:
-        builder.registerOrSetId(deviceId=args.device_id)
+    if config.args.device_id != None:
+        builder.registerOrSetId(deviceId=config.args.device_id)
     else:
         builder.registerDevice()
 
-    _iobeamClient = builder.build()
+    config.iobeamClient = builder.build()
+    config.iobeamDataStore = config.iobeamClient.createDataStore(config.format)
 
-    if args.repetitions == 0:
-        analyzeFile(args)
-    elif args.repetitions > 0:
-        while args.repetitions > 0:
-            analyzeFile(args)
-            args.repetitions -= 1
-    else:
-        _parser.print_usage()
-        print "\nError: Repetitions must be >= 0."
-        sys.exit(1)
+    if config.args.repetitions == 0:
+        analyzeFile(config)
+    elif config.args.repetitions > 0:
+        while config.args.repetitions > 0:
+            analyzeFile(config)
+            config.args.repetitions -= 1
